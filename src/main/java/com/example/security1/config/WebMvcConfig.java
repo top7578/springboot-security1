8218da8c0a38c648cbd9993ae2f592a046c88287
junwoo <top7578@naver.com>
2022-07-30 16:39:23 +0900

    [FEAT] 시큐리티 설정

--- src/main/java/com/example/security1/config/WebMvcConfig.java
+++ src/main/java/com/example/security1/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.example.security1.config;
 
+
 import org.springframework.boot.web.servlet.view.MustacheViewResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
